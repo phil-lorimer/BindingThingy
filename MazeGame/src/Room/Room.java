66d4662571f4f9a8aea5e4859115e4f18d2b47b6
phil-lorimer <phil.lorimer@gmail.com>
2020-10-02 10:37:50 -0400

    Update Room.java

--- MazeGame/src/Room/Room.java
+++ MazeGame/src/Room/Room.java
@@ -1,12 +1,19 @@
 package Room;
 
-public class Room {
+public abstract class Room {
+	Room above;
+	Room below;
+	Room left;
+	Room right;
 
-	public Room() {
-		
+	abstract Room() {
 	}
-	public void getAbove(){}
-	public void getBelow(){}
-	public void getLeft(){}
-	public void getRight(){}
+	abstract void setAbove(Room aboveRoom);
+	abstract void setBelow(Room belowRoom);
+	abstract void setLeft(Room leftRoom);
+	abstract void setRight(Room rightRoom);
+	abstract void getAbove();
+	abstract void getBelow();
+	abstract void getLeft();
+	abstract void getRight();
 }
